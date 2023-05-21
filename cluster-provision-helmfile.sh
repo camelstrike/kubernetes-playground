988f81e816e9e5cb3638f4cb11dc0566be49c8d9
camelstrike <gmdibiase@gmail.com>
2023-05-21 10:03:34 +0200

    add cond checks and export argocd init password

--- cluster-provision-helmfile.sh
+++ cluster-provision-helmfile.sh
@@ -2,7 +2,18 @@
 
 set -e
 
-# Kind
+helmfile_run() {
+ HELMFILE_APPLY=$(helmfile apply)
+
+ if [[ ${HELMFILE_APPLY} ]]; then
+   ARGOCD_INITIAL_PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+   echo -e "\nTo continue go to https://argocd.local"
+   echo "Username: admin"
+   echo -e "Password: ${ARGOCD_INITIAL_PASS}\n"
+ fi
+}
+
+# Set up Kind cluster
 echo "Setting up Kind cluster"
 if [[ $(kind get clusters) == "kind" ]]; then
 	echo "Cluster kind exists already"
@@ -11,4 +22,5 @@ else
 	kind create cluster --config "${PWD}"/kind/kind-config.yaml --kubeconfig ~/.kube/config
 fi
 
-helmfile apply
+# Bootstrap kind cluster with helmfile
+helmfile_run
